20b4874761b9a9d35d824cc10e6c212aad04ad6a
KRecursive <kenneth.roubal@gmail.com>
2016-09-06 12:49:00 -0400

    My completed coding assignment
    I believe

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Overall Description
+## Computing the inverse of a matrix (using caching).
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## Caching function with placeholders.
+makeCacheMatrix <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve(x) %*% x
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Returns a matrix that is the inverse of 'x'.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setinverse(m)
+        m
 }
+
+
+## Procedures for testing the functions
+
+## My super simple matrix test
+## mat<-matrix(1:4, 2, 2)
+## cacheSolve(makeCacheMatrix(mat))
+## Computed matrix inverse by hand to verify functioning
+
+## My 5x5 matrix
+## mat<-matrix(rnorm(5^2), 5)
+## cacheSolve(makeCacheMatrix(mat))
+
+
